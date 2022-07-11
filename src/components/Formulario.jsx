c1cfdfbbb762f87445710f4d3955431204b276c0
webstylejordi <webstylejordi@gmail.com>
2022-07-11 08:26:31 +0200

    formulario validado

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,14 +1,37 @@
 import React from 'react'
-import {Formik, Form, Field} from 'formik'
-
+import {Formik, Form, Field } from 'formik'
+import * as Yup from 'yup'
+import Alerta from './Alerta'
 
 export const Formulario = () => {
+
+     const handleSubmit =(valores) => {
+          console.log('llego aqui')
+          console.log(valores)
+     }
+
+     const nuevoClienteShema = Yup.object().shape({
+          nombre : Yup.string()
+                      .required('el nombre del cliente es obligatorio')
+                      .min(3,'el nombre es muy corto'),
+          empresa : Yup.string()
+                      .required('la empresae es obligatoria'),
+          mail :  Yup.string()
+                     .required('se necesita un email')
+                     .email('formato incorrecto de email'),
+          telf :  Yup.number()
+                     .positive('Numero no valido')
+                     .integer('Numero no válido')
+                     .typeError('El número es incorrecto'),
+          notas :''
+})
+
   return (
     <div className='bg-white mt-10 px-5 py-10 shadow-md rounded-md md:w-3/4 mx-auto'>
           <h1 className='font-bold  text-gray-600 text-xl uppercase text-center'>
                Agregar Cliente
           </h1>
-
+ 
           <Formik
                initialValues={{
                     nombre : '',
@@ -17,8 +40,14 @@ export const Formulario = () => {
                     telf : '',
                     notas :''
                }}
+               onSubmit={(values) =>(
+                    handleSubmit(values)
+               ) }
+               validationSchema= {nuevoClienteShema}
           >
-               {() => (
+               {({errors, touched}) => { 
+                    return (
+
                <Form className='mt-10'>
                     <div className='mb-4'>
                          <label
@@ -29,10 +58,13 @@ export const Formulario = () => {
                          <Field 
                               id="nombre"
                               name="nombre"
-                              type="password"
+                              type="text"
                               placeholder="nombre cliente"
                               className="mt-2 block w-full p-3 bg-gray-50"
                          />
+                          {errors.nombre && touched.nombre ? (
+                              <Alerta>{errors.nombre}</Alerta>  )
+                              :null}
                     </div >
 
                     <div className='mb-4' >
@@ -48,6 +80,9 @@ export const Formulario = () => {
                               placeholder="Empresa del Cliente"
                               className="mt-2 block w-full p-3 bg-gray-50"
                          />
+                        {errors.empresa && touched.empresa ? (
+                              <Alerta>{errors.empresa}</Alerta>  )
+                              :null}
                     </div>
                  
                     <div className='mb-4'>
@@ -63,6 +98,9 @@ export const Formulario = () => {
                               placeholder="email del Cliente"
                               className="mt-2 block w-full p-3 bg-gray-50"
                          />
+                         {errors.mail && touched.mail ? (
+                              <Alerta>{errors.mail}</Alerta>  )
+                              :null}
                     </div>
 
                     <div className='mb-4'>
@@ -78,6 +116,9 @@ export const Formulario = () => {
                               placeholder="email del Cliente"
                               className="mt-2 block w-full p-3 bg-gray-50"
                          />
+                         {errors.telf && touched.telf ? (
+                              <Alerta>{errors.telf}</Alerta>  )
+                              :null}
                     </div>
 
                     <div className='mb-4'>
@@ -102,7 +143,7 @@ export const Formulario = () => {
                          className='mt-5 w-full p-3 bg-blue-800 text-white uppercase font-bold text-lg text-center'
                     />
                </Form>
-                    )}
+                    )}}
           </Formik>
     </div>
   )
